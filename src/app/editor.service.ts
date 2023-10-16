0dd3f512b53260531377d99b2c571aefa7a32995
Simon Rabuogi <symokevo@gmail.com>
2023-10-16 20:01:04 +0300

    create getter and setter methods in the editor service

--- src/app/editor.service.ts
+++ src/app/editor.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { ElectronService } from 'ngx-electronyzer';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EditorService {
 
-  constructor() { }
+  constructor(private electronService: ElectronService) { }
+
+  getContent(): Promise<string> {
+    return this.electronService.ipcRenderer.invoke('getContent');
+  }
+
+  setContent(content: string) {
+    this.electronService.ipcRenderer.invoke('setContent', content);
+  }
 }
