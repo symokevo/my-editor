8bfa8185ecebfb89635ecc8bf1b67a2d1c02a616
Simon Rabuogi <symokevo@gmail.com>
2023-10-16 05:13:55 +0300

    Import and register ngxwigmodule in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgxWigModule } from 'ngx-wig';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxWigModule
   ],
   providers: [],
   bootstrap: [AppComponent]
