1390abf97ae3ff4dcb5f10ce70abfbd0fbc2c125
Simon Rabuogi <symokevo@gmail.com>
2023-10-16 19:36:28 +0300

    configure node rendering

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -3,7 +3,11 @@ import { app, BrowserWindow } from 'electron';
 function createWindow () {
     const mainWindow = new BrowserWindow({
         width: 800,
-        height: 600
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        }
     });
 
     mainWindow.loadFile('index.html');
